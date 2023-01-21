1fc6c044fbfecac2ad4518305a11f2cf0b95a1b8
kaichu-shishi <54222944+kaichu-shishi@users.noreply.github.com>
2023-01-21 20:11:24 +0900

    Add files via upload

--- sneakers/js/index.js
+++ sneakers/js/index.js
@@ -29,5 +29,23 @@ $(function(){
         slidesToShow: 1, // 表示させるスライド数
         centerMode: true, // slidesToShowが奇数のとき、現在のスライドを中央に表示する
         variableWidth: true, // スライド幅の自動計算を無効化
-      });
+    });
+
+    // スクロール時の画像フェード表示
+    $(window).scroll(function() {
+        // fadeinクラスに対して順に処理を行う
+        $('.js-fadein').each(function() {
+        // スクロールした距離
+        let scroll = $(window).scrollTop();
+        // fadeinクラスの要素までの距離
+        let target = $(this).offset().top;
+        // 画面の高さ
+        let windowHeight = $(window).height();
+        // fadeinクラスの要素が画面下にきてから200px通過したタイミングで要素を表示
+        if (scroll > target - windowHeight + 200) {
+            $(this).css('opacity','1');
+            $(this).css('transform','translateY(0)');
+        }
+        });
+    });
 });
